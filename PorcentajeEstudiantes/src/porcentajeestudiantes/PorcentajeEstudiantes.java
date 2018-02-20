5a787cf496a273ad233301672c0318567170f4c0
Juan José Rangel <36427191+jjrangel09@users.noreply.github.com>
2018-02-19 19:39:41 -0500

    Se crea la primera version funcional del programa
    Codigo para calcular porcentaje de alumnos pertenecientes al grupo de Programacion II

---
+++ PorcentajeEstudiantes/src/porcentajeestudiantes/PorcentajeEstudiantes.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package porcentajeestudiantes;
+
+/**
+ *
+ * @author JuanJ
+ */
+public class PorcentajeEstudiantes {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        float porcentaje = 0;
+        int estudiantes = 240;
+        int menores = 35;
+        
+        porcentaje = (menores*100)/estudiantes;
+        
+        System.out.println(porcentaje);
+    }
+    
+}
